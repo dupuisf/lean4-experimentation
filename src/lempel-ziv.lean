f623dc7c427cd8a235a3c9c11e82f3b038521e86
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2021-03-01 21:54:12 -0500

    Lempel-Ziv, sort of working for lists of Fin 2

--- src/lempel-ziv.lean
+++ src/lempel-ziv.lean
@@ -6,6 +6,7 @@ Authors: Frédéric Dupuis
 
 /-
 A toy Lempel-Ziv compressor and decompressor.
+Only works for `List (Fin 2)`
 -/
 
 inductive LZBlock (α : Type u) [BEq α] where
@@ -34,10 +35,36 @@ def mkEmpty : LZBlock α := last 0 []
 
 instance [ToString α] : ToString (LZBlock α) where
     toString b := match b with
-    | reg n s c => toString (s ++ [c])
-    | last n s  => toString s
+    | reg n s c => "R" ++ toString s ++ toString c
+    | last n s  => "L" ++ toString s
 
 end LZBlock
+
+class HasBitstringRepr (α : Type u) where
+    toBitstring : α → List (Fin 2)
+
+def toBitstr [HasBitstringRepr α] (x : α) : List (Fin 2) :=
+    HasBitstringRepr.toBitstring x
+
+def toBitstrPr [HasBitstringRepr α] (x : α) (pr : Nat) : List (Fin 2) :=
+    let xs := toBitstr x
+    let s := List.replicate pr (0 : Fin 2) ++ xs
+    List.drop (s.length - pr) s
+
+partial def natToBitstring : Nat → List (Fin 2)
+| 0                     => []
+| 1                     => [1]
+| Nat.succ (Nat.succ n) => let n' := n+2
+    if n' % 2 = 1 then (natToBitstring (n'/2)) ++ [1] else (natToBitstring (n'/2)) ++ [0]
+
+instance : HasBitstringRepr Bool where
+    toBitstring := fun n => if n then [1] else [0]
+
+instance : HasBitstringRepr Nat := ⟨natToBitstring⟩
+
+instance {n : Nat} : HasBitstringRepr (Fin n) := ⟨natToBitstring ∘ Coe.coe⟩
+
+instance : HasBitstringRepr Char := ⟨natToBitstring ∘ Char.toNat⟩
 
 /-- Segment the input string into LZ blocks -/
 def toBlocks (curList : List (LZBlock α)) (curBlock : List α) : List α → List (LZBlock α)
@@ -47,40 +74,31 @@ def toBlocks (curList : List (LZBlock α)) (curBlock : List α) : List α → Li
     | none   => toBlocks (curList ++ [reg curList.length curBlock x]) [] xs
     | some b => toBlocks curList nextBlk xs
 
-def getPrefixId (lst : List (LZBlock α)) (b : List α) : Nat :=
+def getPrefixId (lst : List (LZBlock α)) (b : LZBlock α) : Nat :=
     let lstn := List.enum lst
-    match lstn.find? (fun blk => blk.2.getStr == b) with
+    match lstn.find? (fun blk => blk.2.getStr == b.getPrefix) with
     | none => panic! "Help!"
     | some blk => blk.1
 
---inductive Bit
---| Zero
---| One
---
---open Bit
---
---instance : BEq Bit :=
---    ⟨fun a b => match a, b with
---    | Zero, Zero => true
---    | Zero, One  => false
---    | One, Zero  => false
---    | One, One   => true⟩
---
---instance : ToString Bit where
---    toString b := match b with
---    | Zero => "0"
---    | One => "1"
---
---def listBitToString : List Bit → String
---| []        => ""
---| (x :: xs) => (toString x) ++ (listBitToString xs)
+def compressBlocks [HasBitstringRepr α] (s : List α) : List (List (Fin 2)) :=
+    let blks := toBlocks [mkEmpty] [] s
+    let ids := List.map (getPrefixId blks) blks
+    let ids_enum := List.enum ids
+    let f : LZBlock α → Nat × Nat → List (Fin 2)
+    | last _ str,  (k₁, k₂)  => toBitstrPr k₂ (toBitstr (k₁ - 1)).length
+    | reg _ str c,  (k₁, k₂) => (toBitstrPr k₂ (toBitstr (k₁ - 1)).length) ++ (toBitstrPr c 1)
+    List.map₂ f blks ids_enum
 
 def main : IO Unit :=
-do let str := "aaaaaabbbbbbbbbbbb"
-   IO.print $ "Voici une chaîne: " ++ str ++ "\n"
-   let strLZ := toBlocks [mkEmpty] [] str.toList
-   --IO.print $ List.map getPrefixId $ toBlocks [mkEmpty] [] str.toList
-   IO.print "\n"
+do let str := [0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1]
+   let strLZ := toBlocks [mkEmpty] [] str
    IO.print strLZ
+   IO.print "\n"
+   IO.print $ List.map (getPrefixId strLZ) $ strLZ
+   IO.print "\n"
+   IO.print $ compressBlocks str
+   IO.print "\n"
+   IO.print $ toBitstr 0
+
 
 #eval main
